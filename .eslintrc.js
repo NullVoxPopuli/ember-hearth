86a2fe641e87eb9a1ca88ded7319f6580e17b704
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-08-10 21:16:24 -0400

    blind upgrade to latest 2.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true
-    }
+    ecmaVersion: 2017,
+    sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
-    'browser': true,
+    browser: true
   },
   globals: {
     requireNode: true,
@@ -21,4 +24,32 @@ module.exports = {
     'one-var': ['error', 'never'],
     'indent': ['error', 2]
   },
+  overrides: [
+    // node files
+    {
+      files: [
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    },
+
+    // test files
+    {
+      files: ['tests/**/*.js'],
+      excludedFiles: ['tests/dummy/**/*.js'],
+      env: {
+        embertest: true
+      }
+    }
+  ]
 };
